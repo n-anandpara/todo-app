5e169638649353c085de4ce17f478c69983630dc
Neha Anandpara <neha.anandpara1234@gmail.com>
2019-12-13 23:55:59 +0530

    Bootstrap added and custom styles updated. Todo title, todo list renderer and input field added.

--- src/components/ListRenderer.js
+++ src/components/ListRenderer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../styles/ListRendererStyle.css';
 
-export default function ListRenderer() {
+const ListRenderer = ({dataList = []}) => {
     return (
-        <div>
-            Hi I am in List Renderer...
-        </div>
+        dataList.map((element, index) => {
+            return <div key={index} > {element.label} </div>
+        })
     )
 }
+
+export default ListRenderer;
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react'
 import ListRenderer from './ListRenderer';
+import '../styles/TodoAppStyle.css';
 
 class TodoApp extends Component {
-    render() {
+    constructor(props) {
+        super(props)
+        this.state = { list: [], value: '' };
+        this.inputRef = React.createRef();
+    }
+
+    getTitle = () => (
+        <div className="row header pb-1">
+            <div className="col-6 title" >
+                Todo
+            </div>
+            <div className='col-6 d-flex justify-content-end' >
+                <button type="button" className="btn btn-secondary btn-sm my-2">
+                    <i className="fa fa-plus"></i>
+                </button>
+            </div>
+        </div>
+    );
+
+    addElement = (element) => {
+        const { list } = this.state;
+        let tempList = list;
+        tempList.push({
+            label: element,
+            complete: false
+        });
+        this.setState({ list: tempList });
+    }
+
+    addElementOnEnter = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            this.setState({ value: '' });
+            this.addElement(event.target.value);
+        }
+    }
+
+    getInputField = () => {
+        const { value } = this.state;
         return (
-            <div>
-                <ListRenderer />
-                I am in todo App...
+            <form className='py-2'>
+                    <input
+                        value={value}
+                        className="input-class form-control-plaintext"
+                        ref={this.inputRef}
+                        autoFocus
+                        onChange={e => this.setState({ value: e.target.value})}
+                        onKeyPress={this.addElementOnEnter}
+                    />
+                </form>
+        )
+    }
+
+    render() {
+        const { list } = this.state;
+        return (
+            <div className="container body">
+                {this.getTitle()}
+                <div className="mx-3">
+                    <ListRenderer dataList={list}/>
+                    {this.getInputField()}
+                </div>
             </div>
         )
     }
